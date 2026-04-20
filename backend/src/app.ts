41bf505fb4bfd391e70070e145d024b21f76712c
anwrat <anweshrwt22@gmail.com>
2026-04-20 13:43:57 +0545

    Creating a simple error handling middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,11 @@
 import express from 'express';
+import { errorHandler } from './middleware/errorHandler.middleware.js';
 
 const app = express();
 app.use(express.json());
+
+//Routes
+
+app.use(errorHandler);
 
 export default app;
